719f93b85ad8cf3be4e368d2df3b3f34d3bfed66
Brent Goodrick <bgoodr@gmail.com>
2015-05-24 13:19:23 -0700

    Fix formatting by using spaces and an Emacs mode setting
    Otherwise, the formatting looks weird when rendered by GitHub.

--- project-03/project-03.ino
+++ project-03/project-03.ino
@@ -1,46 +1,48 @@
+// -*-mode: c++; indent-tabs-mode: nil; -*-
+
 const int sensorPin = A0;
 const float baselineTemp = 23.73; // <-- change this value to the average value you see from the serial monitor before touching the TMP36
 
 
 void setup() {
-	Serial.begin(9600); // open a serial port
-	Serial.println("Setting up the pins ..."); // <-- not a part of the original starter kit project code!
-	for (int pinNumber = 2; pinNumber<5; pinNumber++) {
-		pinMode(pinNumber, OUTPUT);
-		digitalWrite(pinNumber, LOW);
-	}
+  Serial.begin(9600); // open a serial port
+  Serial.println("Setting up the pins ..."); // <-- not a part of the original starter kit project code!
+  for (int pinNumber = 2; pinNumber<5; pinNumber++) {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
 }
 
 void loop() {
-	int sensorVal = analogRead(sensorPin);
-	Serial.print("Sensor Value: ");
-	Serial.print(sensorVal);
-	// convert the ADC reading to voltage:
-	float voltage = (sensorVal/1024.0) * 5.0;
-	Serial.print(", Volts: ");
-	Serial.print(voltage);
-	Serial.print(", degrees C: ");
-	// convert the voltage to temperature in degrees:
-	float temperature = (voltage - .5) * 100;
-	Serial.println(temperature);
-	if (temperature < baselineTemp) {
-		digitalWrite(2, LOW);
-		digitalWrite(3, LOW);
-		digitalWrite(4, LOW);
-	} else if (temperature >= baselineTemp+2 &&
-						 temperature < baselineTemp+4) {
-		digitalWrite(2, HIGH);
-		digitalWrite(3, LOW);
-		digitalWrite(4, LOW);
-	} else if (temperature >= baselineTemp+4 &&
-						 temperature < baselineTemp+6) {
-		digitalWrite(2, HIGH);
-		digitalWrite(3, HIGH);
-		digitalWrite(4, LOW);
-	} else if (temperature > baselineTemp+6) {
-		digitalWrite(2, HIGH);
-		digitalWrite(3, HIGH);
-		digitalWrite(4, HIGH);
-	}
-	delay(1);
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+  // convert the ADC reading to voltage:
+  float voltage = (sensorVal/1024.0) * 5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  Serial.print(", degrees C: ");
+  // convert the voltage to temperature in degrees:
+  float temperature = (voltage - .5) * 100;
+  Serial.println(temperature);
+  if (temperature < baselineTemp) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp+2 &&
+             temperature < baselineTemp+4) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp+4 &&
+             temperature < baselineTemp+6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  } else if (temperature > baselineTemp+6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(1);
 }
